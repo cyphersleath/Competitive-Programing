6b3e05bb4962dea70a88011d60d966ad0973ffca
Cypher <cyphersleath@gmail.com>
2024-08-08 12:27:40 +0530

    Updated NewtonSchcoolTQ
    Problem 3 has been added

--- Newton-School-Todays-Question/problem-solution.py
+++ Newton-School-Todays-Question/problem-solution.py
@@ -40,3 +40,15 @@ t=int(input())
 for _ in range(t):
     n,k=map(int,input().split())
     print((n//k)**2)
+
+#Problem 3
+'''
+Your teacher gave you an assignment - given an integer N, construct a binary string B = b1​b2​b3​…bN​ of length N such that: max(bi​, bi+1​) = 1 for every i from 1 to N - 1.
+What is the minimum number of 1's such a binary string can contain?
+Note: A binary string is a string consisting of only the digits 0 and 1.
+'''
+#Solution
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    print(n//2)
