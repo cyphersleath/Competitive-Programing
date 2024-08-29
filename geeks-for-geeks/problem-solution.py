40a0d45d4bad6e7454a6b6095466d0d24eec7c27
Cypher <cyphersleath@gmail.com>
2024-08-29 13:36:30 +0530

    Geeksforgeeks updated
    Problem 6 to 9 updated

--- geeks-for-geeks/problem-solution.py
+++ geeks-for-geeks/problem-solution.py
@@ -50,16 +50,36 @@ class Solution:
 
 #Problem 5
 '''
-For an integer N find the number of trailing zeroes in N!.'''
+Given an 0-indexed array of integers arr[] of size n, find its peak element and return it's index. An element is considered to be peak if it's value is greater than or equal to the values of its adjacent elements (if they exist).
+
+Note: The output will be 1 if the index returned by your function is correct; otherwise, it will be 0.'''
+
 #Solution
+class Solution:   
+    def peakElement(self,arr, n):
+        l=sorted(arr)
+        return arr.index(l[-1])  
+#Problem 6
+'''
+Given an array arr. Your task is to find the minimum and maximum elements in the array.
+
+Note: Return an array that contains two elements the first one will be a minimum element and the second will be a maximum of an array'''
+#Problem 7
 class Solution:
-    def trailingZeroes(self, N):
-        n=N
-        if (n<0):
-            return -1
-        count =0
-        while(n>=5):
-            n//=5
-            count+=n
-        return count
+    def get_min_max(self, arr):
+        max=0
+        min = arr[0]
+        for i in arr:
+            if min >=i:
+                min = i
+            if max < i:
+                max = i
+        return min,max
+#Problem 8
+'''
+You are given a string s. You need to reverse the string.'''
+#Problem 9
+class Solution:
+     def reverseWord(self, str: str) -> str:
+         return str[::-1]
 
